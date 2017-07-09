834d15ba7572c9b2e3c69c24c39b14ce66917c93
Massink <jasper.massink@capgemini.com>
2017-07-09 23:25:18 +0200

    stap 6 reactive transformations

--- src/app/contact-list/add-contact/add-contact.component.ts
+++ src/app/contact-list/add-contact/add-contact.component.ts
@@ -54,7 +54,9 @@ export class AddContactComponent implements OnInit {
     })
 
     const emailControl = this.contactFormModel.get('emailAddress');
-    emailControl.valueChanges.subscribe(value => this.setEmailValidationMessage(emailControl))
+    // Debounce time reactive transformation added
+    emailControl.valueChanges.debounceTime(1000).subscribe(value => this.setEmailValidationMessage(emailControl));
+    //  emailControl.valueChanges.subscribe(value => this.setEmailValidationMessage(emailControl));
   }
 
   setEmailValidationMessage(control: AbstractControl) {
